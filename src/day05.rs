ef35e341298422c67505aa0d3c94af31b594a25f
Richard Rast <richard.rast@trinetx.com>
2024-12-10 13:59:54 -0500

    feat: day 5 b

--- src/day05.rs
+++ src/day05.rs
@@ -1,3 +1,4 @@
+use ahash::{HashMap, HashSet};
 use regex::Regex;
 
 const INPUT_FILE: &str = "input/05.txt";
@@ -14,7 +15,7 @@ fn a_with_input(input: &str) -> u32 {
 
     for numbering in parsed.numberings.iter() {
         if is_list_compatible(&parsed.orderings, numbering.as_slice()) {
-            assert!(numbering.len() > 0, "Must be nonempty");
+            assert!(!numbering.is_empty(), "Must be nonempty");
             assert_eq!(numbering.len() % 2, 1, "Must be odd length");
 
             total += numbering[numbering.len() / 2];
@@ -24,10 +25,10 @@ fn a_with_input(input: &str) -> u32 {
     total
 }
 
-fn is_list_compatible(orderings: &[(u32, u32)], numbering: &[u32]) -> bool {
+fn is_list_compatible(orderings: &HashMap<u32, HashSet<u32>>, numbering: &[u32]) -> bool {
     for i in 0..numbering.len() - 1 {
         for j in i + 1..numbering.len() {
-            if !is_pair_compatible(numbering[i], numbering[j], orderings) {
+            if orderings.get(&numbering[j]).map(|set| set.contains(&numbering[i])).unwrap_or(false) {
                 return false;
             }
         }
@@ -36,31 +37,56 @@ fn is_list_compatible(orderings: &[(u32, u32)], numbering: &[u32]) -> bool {
     true
 }
 
-fn is_pair_compatible(a: u32, b: u32, orderings: &[(u32, u32)]) -> bool {
-    orderings.iter().copied().all(|pair| pair != (b, a))
-}
-
 pub fn b() -> String {
     let input = std::fs::read_to_string(INPUT_FILE).expect("Input should exist");
     b_with_input(&input).to_string()
 }
 
-fn b_with_input(_input: &str) -> String {
-    unimplemented!()
+fn b_with_input(input: &str) -> u32 {
+    let mut parsed = parse(input);
+
+    let mut total = 0;
+
+    for numbering in parsed.numberings.iter_mut() {
+        if !is_list_compatible(&parsed.orderings, numbering.as_slice()) {
+            sort_numbering(&parsed.orderings, numbering);
+            assert!(!numbering.is_empty(), "Must be nonempty");
+            assert_eq!(numbering.len() % 2, 1, "Must be odd length");
+
+            total += numbering[numbering.len() / 2];
+        }
+    }
+
+    total
+}
+
+fn sort_numbering(orderings: &HashMap<u32, HashSet<u32>>, numbering: &mut [u32]) {
+    numbering.sort_by(|a, b| {
+        if orderings.get(a).map(|s| s.contains(b)).unwrap_or(false) {
+            std::cmp::Ordering::Less
+        } else if orderings.get(b).map(|s| s.contains(a)).unwrap_or(false) {
+            std::cmp::Ordering::Greater
+        } else if a == b {
+            std::cmp::Ordering::Equal
+        } else {
+            panic!("I assumed all edges would be supplied, but {a} vs {b} is not")
+        }
+    })
 }
 
 struct Parsed {
-    orderings: Vec<(u32, u32)>,
+    // map x -> (things > x)
+    orderings: HashMap<u32, HashSet<u32>>,
     numberings: Vec<Vec<u32>>,
 }
 
 fn parse(input: &str) -> Parsed {
     let mut lines = input.lines();
 
-    let mut orderings = Vec::new();
+    let mut orderings: HashMap<u32, HashSet<u32>> = HashMap::default();
     let pair_re = Regex::new("^([0-9]+)\\|([0-9]+)$").unwrap();
 
-    while let Some(line) = lines.next() {
+    for line in &mut lines {
         if line.is_empty() {
             break;
         }
@@ -69,12 +95,12 @@ fn parse(input: &str) -> Parsed {
         let num_a: u32 = num_a.parse().unwrap();
         let num_b: u32 = num_b.parse().unwrap();
 
-        orderings.push((num_a, num_b));
+        orderings.entry(num_a).or_default().insert(num_b);
     }
 
     let mut numberings = Vec::new();
 
-    while let Some(line) = lines.next() {
+    for line in lines {
         let numbering: Vec<u32> = line.split(",").map(|token| token.parse::<u32>().unwrap()).collect();
         numberings.push(numbering);
     }
@@ -118,5 +144,39 @@ mod tests {
 97,13,75,29,47";
 
         assert_eq!(a_with_input(input), 143);
+    }
+
+    #[test]
+    fn sample_b() {
+        let input = "47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47";
+
+        assert_eq!(b_with_input(input), 123);
     }
 }
